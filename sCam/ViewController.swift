aeec989732a74912c53c0797c771375b338259a0
takayuki doseki <swift.takayuki@gmail.com>
2021-09-15 19:27:38 +0900

    Initial Commit

---
+++ sCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sCam
+//
+//  Created by 道関孝行 on 2021/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
